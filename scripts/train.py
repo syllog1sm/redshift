992745b433c94fc9a770a00ec99d13e72f4e8d6b
Matthew Honnibal <matthew@honnibal.com>
2013-02-15 14:35:08 +1100

    * Added profiling flag

--- scripts/train.py
+++ scripts/train.py
@@ -6,7 +6,8 @@ import sys
 import plac
 import time
 from pathlib import Path
-
+import pstats
+import cProfile
 
 import redshift.parser
 import redshift.io_parse
@@ -25,17 +26,18 @@ USE_HELD_OUT = False
     shiftless=("Use no shift transition (requires reattach)", "flag", "s", bool),
     repair_only=("Penalise incorrect moves in the oracle even when they can be repaired",
                  "flag", "o", bool),
+    profile=("Run profiler (slow)", "flag", None, bool),
 )
 def main(train_loc, model_loc, train_alg="online", n_iter=15,
          add_extra_feats=False, label_set="Stanford", feat_thresh=1,
          allow_reattach=False, allow_lower=False, shiftless=False,
-         repair_only=False):
+         repair_only=False, profile=False):
     train_loc = Path(train_loc)
-    if allow_reattach:
-        grammar_loc = train_loc.parent().join('rgrammar')
-    else:
-    	grammar_loc = None
     if shiftless:
+        assert allow_reattach
+    if repair_only:
+        assert allow_reattach
+    if allow_lower:
         assert allow_reattach
     model_loc = Path(model_loc)
     if label_set == 'None':
@@ -44,8 +46,7 @@ def main(train_loc, model_loc, train_alg="online", n_iter=15,
                                     train_alg=train_alg, add_extra=add_extra_feats,
                                     label_set=label_set, feat_thresh=feat_thresh,
                                     allow_reattach=allow_reattach, allow_lower=allow_lower,
-                                    grammar_loc=grammar_loc, shiftless=shiftless,
-                                    repair_only=repair_only)
+                                    shiftless=shiftless, repair_only=repair_only)
     if USE_HELD_OUT:
         train_sent_strs = train_loc.open().read().strip().split('\n\n')
         split_point = len(train_sent_strs)/20
@@ -55,7 +56,14 @@ def main(train_loc, model_loc, train_alg="online", n_iter=15,
         to_parse = redshift.io_parse.read_conll('\n\n'.join(train_sent_strs[split_point:]))
     else:
         train = redshift.io_parse.read_conll(train_loc.open().read())
-        parser.train(train, n_iter=n_iter)
+        if profile:
+            print 'profiling'
+            cProfile.runctx("parser.train(train, n_iter=n_iter)", globals(), locals(), "Profile.prof")
+            s = pstats.Stats("Profile.prof")
+            s.strip_dirs().sort_stats("time").print_stats()
+
+        else:
+            parser.train(train, n_iter=n_iter)
         to_parse = redshift.io_parse.read_conll(train_loc.open().read())
     print 'Train accuracy:'
     print parser.add_parses(to_parse, gold=train)
