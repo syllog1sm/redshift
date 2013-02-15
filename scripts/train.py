248d77cb9c74c06d81265fe8d547a98723643e7f
Matthew Honnibal <matthew@honnibal.com>
2013-02-15 22:31:19 +1100

    * Add invert option

--- scripts/train.py
+++ scripts/train.py
@@ -23,6 +23,7 @@ USE_HELD_OUT = False
     feat_thresh=("Feature pruning threshold", "option", "f", int),
     allow_reattach=("Allow left-clobber", "flag", "r", bool),
     allow_lower=("Allow raise/lower", "flag", "w", bool),
+    allow_invert=("Allow invert", "flag", "v", bool),
     shiftless=("Use no shift transition (requires reattach)", "flag", "s", bool),
     repair_only=("Penalise incorrect moves in the oracle even when they can be repaired",
                  "flag", "o", bool),
@@ -30,8 +31,8 @@ USE_HELD_OUT = False
 )
 def main(train_loc, model_loc, train_alg="online", n_iter=15,
          add_extra_feats=False, label_set="Stanford", feat_thresh=1,
-         allow_reattach=False, allow_lower=False, shiftless=False,
-         repair_only=False, profile=False):
+         allow_reattach=False, allow_lower=False, allow_invert=False,
+         shiftless=False, repair_only=False, profile=False):
     train_loc = Path(train_loc)
     if shiftless:
         assert allow_reattach
@@ -45,7 +46,8 @@ def main(train_loc, model_loc, train_alg="online", n_iter=15,
     parser = redshift.parser.Parser(model_loc, clean=True,
                                     train_alg=train_alg, add_extra=add_extra_feats,
                                     label_set=label_set, feat_thresh=feat_thresh,
-                                    allow_reattach=allow_reattach, allow_lower=allow_lower,
+                                    allow_reattach=allow_reattach, allow_invert=allow_invert,
+                                    allow_lower=allow_lower,
                                     shiftless=shiftless, repair_only=repair_only)
     if USE_HELD_OUT:
         train_sent_strs = train_loc.open().read().strip().split('\n\n')
