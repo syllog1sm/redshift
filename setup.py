0f38333fe10ab0a32ad1f957fa3e6c95de8f8024
Matthew Honnibal <matthew@honnibal.com>
2013-03-05 05:58:16 +1100

    * Fixed includes in setup

--- setup.py
+++ setup.py
@@ -24,33 +24,32 @@ def clean(ext):
 
 LIBLINEAR = 'liblinear-weights-1.91'
 pwd = os.path.dirname(__file__)
-
 virtual_env = os.environ.get('VIRTUAL_ENV', '')
+
+includes = [numpy.get_include(),
+            os.path.join(virtual_env, 'include'),
+            os.path.join(pwd, 'index'),
+            os.path.join(pwd, 'svm', 'include')]
+
 exts = [
     Extension('redshift.parser', ["redshift/parser.pyx"], language="c++",
-              include_dirs=[numpy.get_include(), pjoin(virtual_env, 'include')]),
-    Extension('redshift._state', ["redshift/_state.pyx"], language="c++",
-              include_dirs=[numpy.get_include(), os.path.join(virtual_env, 'include')]),
-    Extension('redshift.io_parse', ["redshift/io_parse.pyx"], language="c++"),
+              include_dirs=includes),
+    Extension('redshift._state', ["redshift/_state.pyx"], language="c++"),
+    Extension('redshift.io_parse', ["redshift/io_parse.pyx"], language="c++",
+               include_dirs=includes),
     Extension('redshift.features', ["redshift/features.pyx"], language="c++",
-        
-        include_dirs=[numpy.get_include(), os.path.join(virtual_env, 'include')]
-        ),
+              include_dirs=includes),
     Extension('svm.multitron', ['svm/multitron.pyx'], language="c++",
-            include_dirs=[os.path.join('ext', LIBLINEAR), numpy.get_include(), os.path.join(virtual_env, 'include')], ),
+              include_dirs=includes),
     Extension("svm.cy_svm", ["svm/cy_svm.pyx"], language="c++", libraries=['linear'],
-        include_dirs=[os.path.join('ext', LIBLINEAR), numpy.get_include(),
-                      os.path.join(pwd, 'svm', 'include'),
-                      os.path.join(virtual_env, 'include')],
-        runtime_library_dirs=[os.path.join(pwd, 'svm', 'lib')],
-        library_dirs=[os.path.join(pwd, 'svm', 'lib')],
-        extra_objects=[os.path.join(pwd, 'svm', 'lib', 'liblinear.so.1')]
-    ),
+              include_dirs=includes, runtime_library_dirs=[os.path.join(pwd, 'svm', 'lib')],
+              library_dirs=[os.path.join(pwd, 'svm', 'lib')],
+               extra_objects=[os.path.join(pwd, 'svm', 'lib', 'liblinear.so.1')]),
     Extension(
         "index.hashes",
         ["index/hashes.pyx", "index/MurmurHash2.cpp", "index/MurmurHash3.cpp"],
         language="c++",
-        include_dirs=[numpy.get_include(), os.path.join(virtual_env, 'include')]
+        include_dirs=includes
     )
 ]
 
