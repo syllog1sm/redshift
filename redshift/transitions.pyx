2570bdc271acc01bd22b1b89dee2adf3da0937fe
Matthew Honnibal <matthew@honnibal.com>
2013-07-05 10:11:38 +1000

    * More edit changes, removed commented out arc standard

--- redshift/transitions.pyx
+++ redshift/transitions.pyx
@@ -336,124 +336,12 @@ cdef class TransitionSystem:
             cost += 1
         return cost
     
-    cdef int e_cost(self, State *s, size_t* heads, size_t* labels):
+    cdef int e_cost(self, State *s, size_t* heads, size_t* labels, bint* edits):
         if s.top == 0:
             return -1
-        if heads[s.top] == s.top:
+        if edits[s.top]:
             return 0
         else:
             return 1
 
 
-
-"""
-cdef class ArcStandard(TransitionSystem):        
-    def __cinit__(self, object labels, allow_reattach=False,
-                  allow_reduce=False):
-        self.n_labels = len(labels)
-        self.py_labels = labels
-        self.allow_reattach = allow_reattach
-        self.allow_reduce = allow_reduce
-        self.nr_class = 0
-        max_classes = N_MOVES * len(labels)
-        self.max_class = max_classes
-        self._costs = <int*>calloc(max_classes, sizeof(int))
-        self.labels = <size_t*>calloc(max_classes, sizeof(size_t))
-        self.moves = <size_t*>calloc(max_classes, sizeof(size_t))
-        self.l_classes = <size_t*>calloc(self.n_labels, sizeof(size_t))
-        self.r_classes = <size_t*>calloc(self.n_labels, sizeof(size_t))
-        self.s_id = 0
-        self.l_start = 1
-        self.l_end = 0
-        self.r_start = 0
-        self.r_end = 0
-        self.d_id = 0
-
-    def set_labels(self, left_labels, right_labels):
-        self.left_labels = [self.py_labels[l] for l in sorted(left_labels)]
-        self.right_labels = [self.py_labels[l] for l in sorted(right_labels)]
-        self.labels[self.s_id] = 0
-        self.moves[self.s_id] = <int>SHIFT
-        clas = self.l_start
-        for label in left_labels:
-            self.moves[clas] = <int>LEFT
-            self.labels[clas] = label
-            self.l_classes[label] = clas
-            clas += 1
-        self.l_end = clas
-        self.r_start = clas
-        for label in right_labels:
-            self.moves[clas] = <int>RIGHT
-            self.labels[clas] = label
-            self.r_classes[label] = clas
-            clas += 1
-        self.r_end = clas
-        self.nr_class = clas
-        return clas
-      
-
-    cdef int transition(self, size_t clas, State *s) except -1:
-        cdef size_t head, child, new_parent, new_child, c, gc, move, label
-        move = self.moves[clas]
-        label = self.labels[clas]
-        s.history[s.t] = clas
-        s.t += 1 
-        if move == SHIFT:
-            assert not s.at_end_of_buffer
-            push_stack(s)
-        elif move == LEFT:
-            assert s.stack_len >= 3
-            head = s.top
-            child = s.second
-            add_dep(s, head, child, label)
-            s.stack_len -= 1
-            s.stack[s.stack_len - 1] = s.top
-            s.second = s.stack[s.stack_len - 2]
-        elif move == RIGHT:
-            assert s.stack_len >= 3
-            child = s.top
-            head = s.second
-            add_dep(s, head, child, label)
-            pop_stack(s)
-        else:
-            raise StandardError(clas)
-        if s.i == (s.n - 1):
-            s.at_end_of_buffer = True
-        if s.at_end_of_buffer and s.stack_len == 2:
-            s.is_finished = True
-            add_dep(s, s.i, s.top, 0)
-            pop_stack(s)
-
-    cdef int break_tie(self, State* s, size_t* heads, size_t* labels) except -1:
-        if s.stack_len < 2 and not s.at_end_of_buffer:
-            return self.s_id
-        elif heads[s.top] == s.second and not has_child_in_buffer(s, s.top, heads):
-            return self.r_classes[labels[s.top]]
-        elif heads[s.second] == s.top:
-            return self.l_classes[labels[s.second]]
-        elif s.at_end_of_buffer:
-            return self.nr_class + 1
-        elif heads[s.i] == s.top or heads[s.top] == s.i:
-            return self.s_id
-        elif not has_head_in_stack(s, s.i, heads) and not has_child_in_stack(s, s.i, heads):
-            return self.s_id
-        else:
-            return self.nr_class + 1
-
-    cdef int* get_valid(self, State* s):
-        cdef size_t i
-        cdef int* valid = self._costs
-        if s.stack_len >= 3:
-            validity = 0
-        else:
-            validity = -1
-        for i in range(self.r_start, self.r_end):
-            valid[i] = validity
-        for i in range(self.l_start, self.l_end):
-            valid[i] = validity
-        if s.at_end_of_buffer:
-            valid[self.s_id] = -1
-        else:
-            valid[self.s_id] = 0
-        return valid
-"""
