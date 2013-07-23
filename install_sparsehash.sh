2826eff9ea44de1fc8439b59fe0885fa6d323921
Matthew Honnibal <matthew@honnibal.com>
2013-07-23 20:31:40 +1000

    * Fixed install location of sparsehash library:

--- install_sparsehash.sh
+++ install_sparsehash.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
-if [-n $VIRTUALENV] 
+if [ $VIRTUALENV ] 
   then 
-    dest=$VIRTUALENV
+    dest="$VIRTUALENV"
   else
-    dest=`pwd`
+    dest=`pwd`/ext
 fi
+echo $dest
+exit
 cd /tmp
 wget https://sparsehash.googlecode.com/files/sparsehash-2.0.2.tar.gz
 tar -xzf sparsehash-2.0.2.tar.gz
