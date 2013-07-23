d7fda70499c9bd66041727d87ba47d423e67f440
Matthew Honnibal <matthew@honnibal.com>
2013-07-23 20:35:26 +1000

    * Fixed install script

--- make_virtualenv.sh
+++ make_virtualenv.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
 cd /tmp
-wget http://www.python.org/ftp/python/2.7.2/Python-2.7.2.tgz /tmp
-tar -zxvf Python-2.7.2.tar.gz
-cd Python-2.7.2
+wget http://www.python.org/ftp/python/2.7.5/Python-2.7.5.tgz /tmp
+tar -zxvf Python-2.7.5.tgz
+cd Python-2.7.5
 mkdir ~/.localpython
 ./configure --prefix=$HOME/.localpython
 make
 make install
 cd /tmp
-wget http://pypi.python.org/packages/source/v/virtualenv/virtualenv-1.5.2.tar.gz#md5=fbcefbd8520bb64bc24a560c6019a73c
+wget --no-check-certificate https://pypi.python.org/packages/source/v/virtualenv/virtualenv-1.5.2    .tar.gz
 tar -zxvf virtualenv-1.5.2.tar.gz
 cd virtualenv-1.5.2/
 ~/.localpython/bin/python setup.py install
