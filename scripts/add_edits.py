b615bc868695a73bf29b45cc4f1bb255f565adf2
Matthew Honnibal <honnibal@gmail.com>
2013-08-30 01:46:15 +1000

    * Fixed add_edits script

--- scripts/add_edits.py
+++ scripts/add_edits.py
@@ -1,42 +1,51 @@
 """Add edit tokens to a parsed file, so that it aligns with the gold-standard"""
-
+import sys
 import plac
 
 def add_toks(test, gold):
+    test.reverse()
+    gold.reverse()
     test_idx = 0
     id_map = {}
-    new = []
+    edits = []
     for gold_idx, g in enumerate(gold):
-        g = g.split()
-        t = test[test_idx].split()
-        if g[0] != t[0]:
-            new.append((gold_idx, g[0], g[1]))
+        if test_idx < len(test):
+            t = test[test_idx].split()
         else:
-            new.append(t)
-            id_map[str(test_idx + 1)] = str(gold_idx + 1)
+            t = None
+        if t is None or g != t[1]:
+            edits.append((gold_idx, g))
+        else:
+            id_map[t[0]] = str(len(gold)- (gold_idx + 1))
             test_idx += 1
-    newer = []
-    for t in new:
-        if len(t) == 3:
-            newer.append('\t'.join((t[0], t[1], t[2], t[0], 'erased')))
-        else:
-            t[0] = id_map[t[0]]
-            if t[3] != '0':
-                t[3] = id_map[t[3]]
-    return '\n'.join(newer)
+    new = []
+    for t in test:
+        t = t.split()
+        t[0] = id_map[t[0]]
+        if t[3] != '-1':
+            t[3] = id_map[t[3]]
+        new.append('\t'.join(t))
+    for idx, word in edits:
+        mapped = len(gold) - (idx+1)
+        new.insert(idx, '%d\t%s\tNN\t%d\terased' % (mapped, word, mapped))
+    return '\n'.join(reversed(new))
 
 def main(test, gold):
     test_sents = open(test).read().split('\n\n')
     gold_sents = open(gold).read().split('\n')
     assert len(test_sents) == len(gold_sents)
     for test_sent, gold_sent in zip(test_sents, gold_sents):
+        if not test_sent.strip() and not gold_sent.strip(): continue
         test_toks = test_sent.split('\n')
-        gold_toks = [w.rsplit('/', 1) for w in gold_sent.split()]
-        if len(test_toks) == len(gold_toks):
-            print test_sent
-        else:
+        gold_sent = gold_sent.replace('you/PRP know/VBP', 'you_know/MWE').replace('i/PRP mean/VBP', 'i_mean/MWE')
+        gold_toks = [g.rsplit('/', 1)[0] for g in gold_sent.split()]
+        try:
             test_sent = add_toks(test_toks, gold_toks)
-            print test_sent
+        except:
+            print >> sys.stderr, test_sent
+            print >> sys.stderr, gold_sent
+            raise
+        print test_sent
         print
 
 if __name__ == '__main__':
