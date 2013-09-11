96e944311abcb170cea090dd7db575b11e6c02b1
Matthew Honnibal <matthew@honnibal.com>
2013-09-11 18:05:02 +1000

    * Hacking tagger scripts

--- scripts/train_tagger.py
+++ scripts/train_tagger.py
@@ -9,22 +9,15 @@ import redshift.io_parse
 @plac.annotations(
     iters=("Number of training iterations", "option", "i", int),
     n_sents=("Number of training sentences", "option", "n", int),
-    conll_format=("Read train and test in conll format", "flag", "c", bool)
 )
-def main(model_loc, train_loc, test_loc, iters=5, n_sents=0, conll_format=False):
+def main(model_loc, train_loc, iters=5, n_sents=0):
     tagger = redshift.tagger.GreedyTagger(model_loc, clean=True)
-    if conll_format:
-        train_strs = open(train_loc).read().strip().split('\n\n')
-    else:
-        train_strs = open(train_loc).read().strip().split('\n')
+    train_strs = open(train_loc).read().strip().replace('|', '/').split('\n')
     # Apply limit
     train_strs = train_strs[-n_sents:]
-    if conll_format:
-        train = redshift.io_parse.read_conll('\n\n'.join(train_strs))
-    else:
-        train = redshift.io_parse.read_pos('\n'.join(train_strs), sep='|')
-    tagger.train(train, nr_iter=iters)
+    tagger.train('\n'.join(train_strs), nr_iter=iters)
     tagger.save()
+    """
     test_data = open(test_loc).read()
     if conll_format:
         to_tag = redshift.io_parse.read_conll(test_data)
@@ -40,6 +33,7 @@ def main(model_loc, train_loc, test_loc, iters=5, n_sents=0, conll_format=False)
         gold = redshift.io_parse.read_pos(test_data, sep='|')
     acc, c, n = redshift.io_parse.eval_tags(to_tag, gold)
     print '%.2f' % acc, c, n
+    """
 
 if __name__ == '__main__':
     plac.call(main)
