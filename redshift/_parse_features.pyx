98c9804bd4ffeb1f25f840aaf1b80b60ec02786e
Matthew Honnibal <matthew@honnibal.com>
2013-10-23 09:52:29 +1100

    * Readd cluster features. Not properly debugged.

--- redshift/_parse_features.pyx
+++ redshift/_parse_features.pyx
@@ -9,49 +9,111 @@ from itertools import combinations
 cdef enum:
     S0w
     S0p
+    S0c
+    S0c6
+    S0c4
     
     S0lw
     S0lp
+    S0lc
+    S0lc6
+    S0lc4
+
     S0l2w
     S0l2p
+    S0l2c
+    S0l2c6
+    S0l2c4
 
     S0lew
     S0lep
+    S0lec
+    S0lec6
+    S0lec4
+
     S0l0w
     S0l0p
+    S0l0c
+    S0l0c6
+    S0l0c4
     
     S0rw
     S0rp
+    S0rc
+    S0rc6
+    S0rc4
+
     S0r2w
     S0r2p
+    S0r2c
+    S0r2c6
+    S0r2c4
 
     S0rew
     S0rep
+    S0rec
+    S0rec6
+    S0rec4
+
     S0r0w
     S0r0p
+    S0r0c
+    S0r0c6
+    S0r0c4
     
     N0w
     N0p
+    N0c
+    N0c6
+    N0c4
 
     N0lw
     N0lp
+    N0lc
+    N0lc6
+    N0lc4
+
     N0l2w
     N0l2p
+    N0l2c
+    N0l2c6
+    N0l2c4
 
     N0lew
     N0lep
+    N0lec
+    N0lec6
+    N0lec4
+
     N0l0w
     N0l0p
+    N0l0c
+    N0l0c6
+    N0l0c4
    
     S0hw
     S0hp
+    S0hc
+    S0hc6
+    S0hc4
+
     S0h2w
     S0h2p
+    S0h2c
+    S0h2c6
+    S0h2c4
 
     N1w
     N1p
+    N1c
+    N1c6
+    N1c4
+
     N2w
     N2p
+    N2c
+    N2c6
+    N2c4
     
     S0l
     S0ll
@@ -109,65 +171,133 @@ cdef void fill_context(size_t* context, size_t nr_label, size_t* words,
 
     context[S0w] = words[k.s0]
     context[S0p] = tags[k.s0]
+    context[S0c] = clusters[k.s0]
+    context[S0c6] = cprefix6s[k.s0]
+    context[S0c4] = cprefix4s[k.s0]
+
     # TODO: Bug with these values not being zeroed on 0-valencies. Probably
     # messes up word match features.
     context[S0lw] = words[s0l.kids[0].idx]
     context[S0lp] = tags[s0l.kids[0].idx]
+    context[S0lc] = clusters[s0l.kids[0].idx]
+    context[S0lc6] = cprefix6s[s0l.kids[0].idx]
+    context[S0lc4] = cprefix4s[s0l.kids[0].idx]
+
     context[S0l2w] = words[s0l.kids[1].idx]
     context[S0l2p] = tags[s0l.kids[1].idx]
+    context[S0l2c] = clusters[s0l.kids[1].idx]
+    context[S0l2c6] = cprefix6s[s0l.kids[1].idx]
+    context[S0l2c4] = cprefix4s[s0l.kids[1].idx]
 
     context[S0lew] = words[s0l.edge]
     context[S0lep] = tags[s0l.edge]
+    context[S0lec] = clusters[s0l.edge]
+    context[S0lec6] = cprefix6s[s0l.edge]
+    context[S0lec4] = cprefix4s[s0l.edge]
+    
     context[S0l0w] = words[s0l.first]
     context[S0l0p] = tags[s0l.first]
+    context[S0l0c] = clusters[s0l.first]
+    context[S0l0c6] = cprefix6s[s0l.first]
+    context[S0l0c4] = cprefix4s[s0l.first]
 
     context[S0rw] = words[s0r.kids[0].idx]
     context[S0rp] = tags[s0r.kids[0].idx]
+    context[S0rc] = clusters[s0r.kids[0].idx]
+    context[S0rc6] = cprefix6s[s0r.kids[0].idx]
+    context[S0rc4] = cprefix4s[s0r.kids[0].idx]
+    
     context[S0r2w] = words[s0r.kids[1].idx]
     context[S0r2p] = tags[s0r.kids[1].idx]
+    context[S0r2c] = clusters[s0r.kids[1].idx]
+    context[S0r2c6] = cprefix6s[s0r.kids[1].idx]
+    context[S0r2c4] = cprefix4s[s0r.kids[1].idx]
 
     context[S0rew] = words[s0r.edge]
     context[S0rep] = tags[s0r.edge]
+    context[S0rec] = clusters[s0r.edge]
+    context[S0rec6] = cprefix6s[s0r.edge]
+    context[S0rec4] = cprefix4s[s0r.edge]
+    
     context[S0r0w] = words[s0r.first]
     context[S0r0p] = tags[s0r.first]
+    context[S0r0c] = clusters[s0r.first]
+    context[S0r0c6] = cprefix6s[s0r.first]
+    context[S0r0c4] = cprefix4s[s0r.first]
 
     context[N0w] = words[k.i]
     context[N0p] = tags[k.i]
+    context[N0c] = clusters[k.i]
+    context[N0c6] = cprefix6s[k.i]
+    context[N0c4] = cprefix4s[k.i]
 
     context[N0lw] = words[n0l.kids[0].idx]
     context[N0lp] = tags[n0l.kids[0].idx]
+    context[N0lc] = clusters[n0l.kids[0].idx]
+    context[N0lc6] = cprefix6s[n0l.kids[0].idx]
+    context[N0lc4] = cprefix4s[n0l.kids[0].idx]
+    
     context[N0l2w] = words[n0l.kids[1].idx]
     context[N0l2p] = tags[n0l.kids[1].idx]
+    context[N0l2c] = clusters[n0l.kids[1].idx]
+    context[N0lc6] = cprefix6s[n0l.kids[1].idx]
+    context[N0l2c4] = cprefix4s[n0l.kids[1].idx]
 
     context[N0lew] = words[n0l.edge]
     context[N0lep] = tags[n0l.edge]
+    context[N0lec] = clusters[n0l.edge]
+    context[N0lec6] = cprefix6s[n0l.edge]
+    context[N0lec4] = cprefix4s[n0l.edge]
+    
     context[N0l0w] = words[n0l.first]
     context[N0l0p] = tags[n0l.first]
+    context[N0l0c] = clusters[n0l.first]
+    context[N0l0c6] = cprefix6s[n0l.first]
+    context[N0l0c4] = cprefix4s[n0l.first]
 
     context[N1w] = words[k.i + 1]
     context[N1p] = tags[k.i + 1]
+    context[N1c] = clusters[k.i + 1]
+    context[N1c6] = cprefix6s[k.i + 1]
+    context[N1c4] = cprefix4s[k.i + 1]
+    
     context[N2w] = words[k.i + 2]
     context[N2p] = tags[k.i + 2]
+    context[N2c] = clusters[k.i + 2]
+    context[N2c6] = cprefix6s[k.i + 2]
+    context[N2c4] = cprefix4s[k.i + 2]
     # If there's a label set for s0, then S1 is the head of S0
     context[S0l] = k.Ls0
     if k.Ls0:
         assert k.s1
         context[S0hw] = words[k.s1]
         context[S0hp] = tags[k.s1]
+        context[S0hc] = clusters[k.s1]
+        context[S0hc6] = cprefix6s[k.s1]
+        context[S0hc4] = cprefix4s[k.s1]
         context[S0hl] = k.Ls1
     else:
         context[S0hw] = 0
         context[S0hp] = 0
+        context[S0hc] = 0
+        context[S0hc6] = 0
+        context[S0hc4] = 0
         context[S0hl] = 0
     # Likewise, if both S0 and S1 have labels, then S2 must be S0's grandparent
     if k.Ls0 and k.Ls1:
         assert k.s2
         context[S0h2w] = words[k.s2]
         context[S0h2p] = tags[k.s2]
+        context[S0h2c] = clusters[k.s2]
+        context[S0h2c6] = cprefix6s[k.s2]
+        context[S0h2c4] = cprefix4s[k.s2]
         context[S0h2l] = k.Ls2
     else:
         context[S0h2w] = 0
         context[S0h2p] = 0
+        context[S0h2c] = 0
+        context[S0h2c6] = 0
+        context[S0h2c4] = 0
         context[S0h2l] = 0
 
     context[S0ll] = s0l.kids[0].lab
@@ -324,18 +454,18 @@ label_sets = (
    (N0w, N0ll, N0l2l),
    (N0p, N0ll, N0l2l),
 )
-"""
+
 extra_labels = (
     (S0p, S0ll, S0lp),
     (S0p, S0ll, S0l2l),
     (S0p, S0rl, S0rp),
     (S0p, S0rl, S0r2l),
     (S0p, S0ll, S0rl),
-    (S0p, S0ll, S0l2l, S0l0l),
     (S0p, S0rl, S0r2l),
     (S0hp, S0l, S0rl),
     (S0hp, S0l, S0ll),
 )
+
 """
 edges = (
     (S0rew,),
@@ -351,7 +481,6 @@ edges = (
     (S0p, N0lep)
 )
 
-"""
 stack_second = (
     (S1w,),
     (S1p,),
@@ -380,6 +509,8 @@ history = (
     (m1, m2, m3, m4),
     (m1, m2, m3, m4, m5),
 )
+"""
+
 
 # Koo et al (2008) dependency features, using Brown clusters.
 clusters = (
@@ -443,7 +574,6 @@ disfl = (
 
 
 # After emailing Mark after ACL
-"""
 new_disfl = (
     (next_edit,),
     (next_next_edit,),
