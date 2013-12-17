aa69fbd1cc7817a1d510cf0d22975ed204378605
Matthew Honnibal <matthew@honnibal.com>
2013-12-17 15:56:35 +1100

    * Add yeppp add_into wrapping, and add initialisation code.

--- ext/yeppp.pxd
+++ ext/yeppp.pxd
@@ -1,2 +1,4 @@
 cdef extern from "yeppp_wrapper.h":
     int add_inplace(double* x, const double* y, size_t length)
+    int add_into(double* out, const double* x, const double* y, size_t length)
+    int init_yeppp()
--- ext/yeppp_wrapper.h
+++ ext/yeppp_wrapper.h
@@ -12,4 +12,16 @@ int add_inplace(double* x, const double* y, int length) {
     return status;
 }
 
+int add_into(double* out, const double* x, const double* y, int length) {
+    enum YepStatus status;
+    status = yepCore_Add_V64fV64f_V64f(x, y, out, length);
+    return status;
+}
 
+int init_yeppp() {
+    enum YepStatus status;
+    /* Initialize the Yeppp! library */
+    status = yepLibrary_Init();
+    assert(status == YepStatusOk);
+    return status;
+}
