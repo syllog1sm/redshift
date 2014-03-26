4ca257ed691fe1e4f5ad5803ed302975ed654814
Matthew Honnibal <matthew@honnibal.com>
2014-03-27 10:54:12 +1100

    * Add use_break and use_sbd flags

--- scripts/train.py
+++ scripts/train.py
@@ -13,12 +13,17 @@ import redshift.parser
     beam_width=("Beam width", "option", "k", int),
     feat_set=("Name of feat set [zhang, iso, full]", "option", "x", str),
     n_sents=("Number of sentences to train from", "option", "n", int),
-    train_tagger=("Train tagger alongside parser", "flag", "p", bool)
+    train_tagger=("Train tagger alongside parser", "flag", "p", bool),
+    use_edit=("Use the Edit transition", "flag", "e", bool),
+    use_break=("Use the Break transition", "flag", "b", bool),
+    seed=("Random seed", "option", "s", int)
 )
 def main(train_loc, model_loc, n_iter=15,
          feat_set="zhang", feat_thresh=10,
          n_sents=0,
-         debug=False, seed=0, beam_width=1,
+         use_edit=False,
+         use_break=False,
+         debug=False, seed=0, beam_width=4,
          train_tagger=False):
     if debug:
         redshift.parser.set_debug(True)
@@ -32,7 +37,9 @@ def main(train_loc, model_loc, n_iter=15,
         train_tagger=train_tagger,
         beam_width=beam_width,
         feat_set=feat_set,
-        feat_thresh=feat_thresh
+        feat_thresh=feat_thresh,
+        use_edit=use_edit,
+        use_break=use_break
     )
 
 
