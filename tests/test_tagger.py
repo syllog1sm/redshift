1d0f84cb8f1e15fa58ff38a993b3ee32097715ce
Matthew Honnibal <matthew@honnibal.com>
2014-05-16 09:43:48 +1000

    * Add tagger tests

---
+++ tests/test_tagger.py
@@ -0,0 +1,53 @@
+"""
+Test POS tagger
+"""
+import os.path
+import pytest
+
+
+def local_path(filename):
+    return os.path.join(os.path.dirname(__file__), filename)
+
+
+model_dir = local_path('model')
+train_str = open(local_path('train.10.conll')).read()
+
+
+@pytest.fixture
+def train_dir():
+    import redshift.tagger
+    sent_strs = []
+    for sent_str in train_str.strip().split('\n\n'):
+        sent = []
+        for tok_str in sent_str.strip().split('\n'):
+            fields = tok_str.split()
+            sent.append('%s/%s' % (fields[1], fields[3]))
+        sent_strs.append(' '.join(sent))
+    train_pos = '\n'.join(sent_strs)
+    redshift.tagger.train(train_pos, model_dir)
+    return model_dir
+
+
+@pytest.fixture
+def tagger(train_dir):
+    import redshift.tagger
+    return redshift.tagger.Tagger(train_dir)
+
+
+@pytest.fixture
+def sentence():
+    from redshift.sentence import Input
+    return Input.from_pos('This/?? is/?? a/?? test/?? ./.')
+
+
+def test_tag(tagger, sentence):
+    tagger.tag(sentence)
+    assert sentence.length == 7
+    tokens = list(sentence.tokens)
+    assert tokens[0].word == 'This'
+    assert tokens[1].word == 'is'
+    assert tokens[2].word == 'a'
+    assert tokens[3].word == 'test'
+    assert tokens[4].word == '.'
+    
+    assert tokens[2].tag == 'DT'
