096cdd1ef4de16667f67a7d902c64b71db0acf85
Matthew Honnibal <matthew@honnibal.com>
2014-06-14 22:31:37 +1000

    * Increment state score from the move score directly, allowing the use of different figure of merits if necessary

--- redshift/beam.pyx
+++ redshift/beam.pyx
@@ -90,7 +90,7 @@ cdef class Beam:
             # parents later.
             copy_state(self.beam[self.bsize], self.parents[parent_idx])
             s = self.beam[self.bsize]
-            s.score = data.first 
+            s.score += self.moves[parent_idx][move_idx].score 
             if not is_final(s):
                 t = &self.moves[parent_idx][move_idx]
                 assert t.cost >= 0
